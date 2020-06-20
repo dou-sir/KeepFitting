9037d7184ed9baf932fdcc9a93f9d04da733a73f
haodadadad <gzh1343572674@gmail.com>
2020-06-20 12:07:20 +0800

    2020.6.20

--- app/src/main/java/com/keepfitting/jit/keepfitting/service/UserService.java
+++ app/src/main/java/com/keepfitting/jit/keepfitting/service/UserService.java
@@ -2,20 +2,18 @@ package com.keepfitting.jit.keepfitting.service;
 
 import com.keepfitting.jit.keepfitting.entity.User;
 
-/**
- * Created by 顾志豪 on 2020/6/19.
- */
+
 
 public interface UserService {
 
 
 
-    public boolean userRegist(User user);
+    //public boolean userRegist(User user);
     //重复
 //    public List<User> findUserByPhone(String phone);
     //登录操作
-   public boolean checkLogin(String phone);
+  // public boolean checkLogin(String phone);
 
     //获取uid
-    public int getUid(String uname);
+    //public int getUid(String uname);
 }
