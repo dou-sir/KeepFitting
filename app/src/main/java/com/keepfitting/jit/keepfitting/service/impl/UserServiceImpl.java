73c52120fcf6032f5f6049e8efad50f749c92f1d
haodadadad <gzh1343572674@gmail.com>
2020-06-20 13:10:09 +0800

    2020.6.20

--- app/src/main/java/com/keepfitting/jit/keepfitting/service/impl/UserServiceImpl.java
+++ app/src/main/java/com/keepfitting/jit/keepfitting/service/impl/UserServiceImpl.java
@@ -7,9 +7,7 @@ import com.keepfitting.jit.keepfitting.dao.impl.UserDaoImpl;
 import com.keepfitting.jit.keepfitting.entity.User;
 import com.keepfitting.jit.keepfitting.service.UserService;
 
-/**
- * Created by 顾志豪 on 2020/6/19.
- */
+
 
 public class UserServiceImpl  implements UserService {
 
@@ -17,20 +15,15 @@ public class UserServiceImpl  implements UserService {
     private UserDao userDao ;
     public UserServiceImpl(Context context){
         userDao = new UserDaoImpl(context);}
-    /**
-     *
-     * @param user  传入参数  （EditText 获取到的内容）
-     * @return
-     * @throws Exception
-     */
 
-    @Override
-    public boolean userRegist(User user) {
-        return false;
-    }
-
-    @Override
-    public int getUid(String uname) {
-        return 0;
-    }
+//
+//    @Override
+//    public boolean userRegist(User user) {
+//        return false;
+//    }
+//
+//    @Override
+//    public int getUid(String uname) {
+//        return 0;
+//    }
 }
