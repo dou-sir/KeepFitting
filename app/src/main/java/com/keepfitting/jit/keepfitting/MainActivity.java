24aebcaa3f276c71ffee2cb8c308e8168927dc9a
dou-sir <1403268725@qq.com>
2020-06-22 15:29:41 +0800

    在MainActivity中加了userInfo，可通过此来进行各种活动

--- app/src/main/java/com/keepfitting/jit/keepfitting/MainActivity.java
+++ app/src/main/java/com/keepfitting/jit/keepfitting/MainActivity.java
@@ -12,22 +12,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.keepfitting.jit.keepfitting.entity.User;
 import com.keepfitting.jit.keepfitting.fragments.Figure00Fragment;
 import com.keepfitting.jit.keepfitting.fragments.Figure01Fragment;
+import com.keepfitting.jit.keepfitting.service.UserService;
+import com.keepfitting.jit.keepfitting.service.impl.UserServiceImpl;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView iv_menu;
     private DrawerLayout dl_menu;
     private NavigationView nav_view;
+    private CircleImageView icon_head;
+    private TextView tv_username,tv_usersay;
+    private ImageButton ib_logout;
     private FragmentManager fragmentManager;
+
+    private UserService userService;
+    public static User userinfo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userService = new UserServiceImpl(this);
+        userinfo = new User();
+
+        Intent intent =getIntent();
+        if(intent.getSerializableExtra("user")!=null){
+
+            userinfo = (User) intent.getSerializableExtra("user");
+            System.out.println(userinfo.toString());
+        }else {
+            userinfo = userService.findUserByUstate();
+        }
+
         init();
     }
 
@@ -91,6 +118,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //nav_head
+        View nav_head = nav_view.getHeaderView(0);
+        icon_head = nav_head.findViewById(R.id.icon_head);
+        tv_username = nav_head.findViewById(R.id.tv_username);
+        tv_usersay = nav_head.findViewById(R.id.tv_usersay);
+        ib_logout = nav_head.findViewById(R.id.ib_logout);
+
+        //设置hi name
+        String hiName = "Hi,";
+        if(userinfo.getNickname()!=null){
+            if (userinfo.getNickname().length() > 11)
+                hiName += userinfo.getNickname().substring(0,10)+"...";
+            else hiName += userinfo.getNickname();
+        }
+        tv_username.setText(hiName);
+        //设置saysomething
+        String saysth = "KEEP+APPLE=Kepple";
+        if(userinfo.getUserID()!=0 || userinfo.getPhone()!=null){
+            saysth = "";
+            if(userinfo.getUserID()!=0) saysth+= "ID:"+userinfo.getUserID()+"";
+            if(userinfo.getPhone()!=null) saysth+= "       TEL:"+userinfo.getPhone();
+        }
+        tv_usersay.setText(saysth);
+
+        icon_head.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo 跳到个人信息
+            }
+        });
+
+        ib_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo 登出
+            }
+        });
+
         //初始化fragment
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         ShowFigureFragment showFigureFragment = new ShowFigureFragment();
@@ -100,6 +165,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
 
     private void fmTransaction(Fragment fragment){
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
