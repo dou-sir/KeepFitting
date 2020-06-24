ee75962d2052e45e15f3decdf532933f70dca603
haodadadad <gzh1343572674@gmail.com>
2020-06-24 15:05:25 +0800

    用户登录功能完善

--- app/src/main/java/com/keepfitting/jit/keepfitting/KonwMeActivity.java
+++ app/src/main/java/com/keepfitting/jit/keepfitting/KonwMeActivity.java
@@ -5,18 +5,24 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.keepfitting.jit.keepfitting.entity.User;
 import com.keepfitting.jit.keepfitting.service.UserService;
 import com.keepfitting.jit.keepfitting.service.impl.UserServiceImpl;
 
+import java.util.Calendar;
+
 public class KonwMeActivity extends AppCompatActivity {
 
     private TextView tv_knowme_bmi,tv_konwme_fangwei,tv_konwme_tizhong,tv_konwme_reliang;
     private UserService userService;
     public static User userinfo;
-
+    private float standardWeight;
+    private BodyData bodyData;
+    private float high;
+    private Button bt_know_back;
 
 
 
@@ -38,9 +44,13 @@ public class KonwMeActivity extends AppCompatActivity {
         }
 
 
+
         init();
 
     }
+//    public float getHigh() {
+//        return high;
+//    }
 
     private void init(){
 
@@ -48,19 +58,45 @@ public class KonwMeActivity extends AppCompatActivity {
         tv_konwme_fangwei=findViewById(R.id.tv_konwme_fangwei);
         tv_konwme_tizhong=findViewById(R.id.tv_konwme_tizhong);
         tv_konwme_reliang=findViewById(R.id.tv_konwme_reliang);
+        bt_know_back=findViewById(R.id.bt_know_back);
+
+
+
 
         //设置BMI
-        String BMI = "55";
-            BMI = userinfo.getBMI()+"";
-
-
-
+        String BMI ;
+        BMI= userinfo.getBMI()+"";
         tv_knowme_bmi.setText(BMI);
 
 
 
+//        Calendar cal = Calendar.getInstance();
+//
+//        cal.setTime(birthDay);
+//        int yearBirth = cal.get(Calendar.YEAR);
+//
+//
 
 
+
+
+
+
+
+
+
+
+        bt_know_back=findViewById(R.id.bt_know_back);
+        bt_know_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(KonwMeActivity.this,MainActivity.class);
+                startActivity(intent);
+                KonwMeActivity.this.finish();
+            }
+        });
     }
+
+
 
 }
